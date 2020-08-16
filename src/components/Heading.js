629c92364a30d3f070c0fd88309417c7e6d41089
Angel Martinez <angel.codes139@gmail.com>
2020-08-16 17:45:06 -0500

    Heading

---
+++ src/components/Heading.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import tw from 'twin.macro';
+
+// Styled Components
+const Container = tw.header`flex justify-center w-full items-center`;
+const Title = tw.h1`text-4xl text-center bg-white px-4 py-2 font-bold text-gray-800`;
+
+const Heading = () => {
+    return (
+        <Container>
+            <Title>Markdown Preview</Title>
+        </Container>
+    );
+}
+ 
+export default Heading;
---
+++ src/components/Markdown.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MarkdownBox = () => {
+    return (
+        <h1>Markdown</h1>
+    );
+}
+
+export default MarkdownBox;
